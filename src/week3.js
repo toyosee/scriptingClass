36c9bfc60ffdb905d222b29e985fcb8e3e7db38f
Elijah Abolaji <tyabolaji@gmail.com>
2024-02-05 20:38:29 +0100

    Number replicator

---
+++ src/week3.js
@@ -0,0 +1,13 @@
+// let food = "akpu"
+// // let beginning = food.at(-1) // CharAt(food.length-1)
+// let beginning = food[0]
+// food[0] = "w"
+// console.log(beginning)
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(2, 22);
+// console.log(part)
+
+let text = "I am sorry\n"
+let more = `${text.repeat(100)}`
+console.log(more)
